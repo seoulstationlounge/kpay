5ccc887eab67b211dade42e4aa62419067ed9a60
Jun <p_illusion@naver.com>
2020-06-29 12:23:41 +0900

    [refactor] api에 대한 설명 추가

--- src/main/java/com/homework/pay/controller/KpayApiController.java
+++ src/main/java/com/homework/pay/controller/KpayApiController.java
@@ -27,13 +27,18 @@ class KpayApiController {
                               @RequestHeader(value = "X-USER-ID") int xUserId,
                               @RequestHeader(value = "X-ROOM-ID") String xRoomId) {
 
+        // 돈 뿌리기의 모든 기능을 담고 있는 separate 객체 입니다.
         SeparateMoney separateMoney = SeparateMoney.of(xRoomId, Kmoney.of(kpay.getCurMoney()), xUserId);
+
+        // token을 util에 MakeRandom 객체를 통해 내려 받습니다.
         String token = KpayMakeRandom.makeToken();
 
+        // token이 기존에 있다면 재 발급 받습니다.
         while (kpayRepository.findByOrOriginUserIdAndTokenDesc(xUserId, token) != null) {
             token = KpayMakeRandom.makeToken();
         }
 
+        // 처음 뿌리기 기능은 초기 데이터이기 때문에 단순히 insert합니다. 추후 에는 token을 기반으로 데이터를 쌓습니다.
         kpayRepository.save(Kpay.builder()
                 .originUserId(separateMoney.getCurrentKuser().getId())
                 .roomId(separateMoney.getRoomNumber())
@@ -53,9 +58,13 @@ class KpayApiController {
                              @RequestHeader(value = "X-USER-ID") int xUserId,
                              @RequestHeader(value = "X-ROOM-ID") String xRoomId) {
 
+        // 돈 뿌리기의 모든 기능을 담고 있는 separate 객체 입니다.
         SeparateMoney separateMoney = SeparateMoney.of(xRoomId, Kmoney.of(kpay.getCurMoney()), xUserId);
 
+        // 기존에 token이 있는지 체크하기 위한 DB조회 입니다.
         Kpay originKpay = kpayRepository.findByOrOriginUserIdAndTokenDesc(separateMoney.getCurrentKuser().getId(), kpay.getToken());
+
+        // 기존에 token에서 내가 받은 내역이 있는지에 대한 DB조회 입니다.
         Kpay findKpay = kpayRepository.findByUserIdAndToken(separateMoney.getCurrentKuser().getId(), kpay.getToken());
 
         if (originKpay != null) {
@@ -66,8 +75,10 @@ class KpayApiController {
             throw new KpayAlreadyGotMoneyException();
         }
 
+        // 랜덤으로 받을 돈을 1000원단위로 받습니다. (랜덤이므로 돈이 남을 수 도 있습니다.)
         int getMoney = separateMoney.getRandomMoney();
 
+        // 기존에 토큰을 index로 받은 내역을 한곳에 쌓습니다. 추후 검색 내역도 같은 table에서 token기반으로 검색 합니다.
         return kpayRepository.save(Kpay.builder()
                 .originUserId(kpay.getOriginUserId())
                 .roomId(separateMoney.getRoomNumber())
